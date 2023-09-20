78db7137fa488b679cb5940a51b676e961dca2af
Juan Carlos <jruiz3416@gmail.com>
2023-09-20 10:37:07 +0200

    Creada funcion imagenFondo

--- app/src/main/java/com/example/tarjetapresentacion/MainActivity.kt
+++ app/src/main/java/com/example/tarjetapresentacion/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.tarjetapresentacion
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tarjetapresentacion.ui.theme.TarjetaPresentacionTheme
 
@@ -30,17 +33,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(nombre: String, email: String, telefono: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello $nombre!",
         modifier = modifier
     )
+}
+
+
+@Composable
+fun imagenFondo(nombre: String, email: String, telefono: String, modifier: Modifier = Modifier){
+    val imgFondo = painterResource(R.drawable.grupo4)
+    Box{
+        Image(
+            painter = imgFondo,
+            contentDescription = null
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TarjetaPresentacionTheme {
-        Greeting("Samu")
+        imagenFondo("Samu","samuelito14@gmail.com","(+34)432251235")
     }
 }
