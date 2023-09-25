73c78b59cb5e3975aca8f5397a8c3c1e866f48fc
Cesar Garcia <cesargarciaviana@gmail.com>
2023-09-26 00:12:02 +0200

    Tarjeta de presentacion (falta ajustar ultima columna)

--- app/src/main/java/com/example/tarjetapresentacion/MainActivity.kt
+++ app/src/main/java/com/example/tarjetapresentacion/MainActivity.kt
@@ -4,15 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tarjetapresentacion.ui.theme.TarjetaPresentacionTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,42 +34,120 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TarjetaPresentacionTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-//                    Greeting("Samu")
-                }
+                Tarjeta()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(nombre: String, email: String, telefono: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $nombre!",
-        modifier = modifier
-    )
-}
-
-
-@Composable
-fun imagenFondo(nombre: String, email: String, telefono: String, modifier: Modifier = Modifier){
-    val imgFondo = painterResource(R.drawable.grupo4)
-    Box{
-        Image(
-            painter = imgFondo,
-            contentDescription = null
-        )
-    }
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    TarjetaPresentacionTheme {
-        imagenFondo("Samu","samuelito14@gmail.com","(+34)432251235")
+fun Tarjeta() {
+    val imgFondo = painterResource(R.drawable.ic_launcher_background)
+    val imgLogo = painterResource(R.drawable.grupo4)
+    val imgMail = painterResource(R.drawable.email)
+    val imgShare = painterResource(R.drawable.compartir)
+    val imgPhone = painterResource(R.drawable.tlf)
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image( //BACKGROUND
+            painter = imgFondo,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            //LOGO
+            Image(
+                painter = imgLogo,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(350.dp)
+                    .clip(shape = MaterialTheme.shapes.medium)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            //TITULO
+            Text(
+                text = "SOMOS EL GRUPO 4",
+                fontSize = 30.sp,
+                color = Color.White
+            )
+            //SUBTITULO
+            Text(
+                text = "Samuel, César y Juan Carlos",
+                fontSize = 22.sp,
+                color = Color.White
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {//ICONO TLF
+                Column {
+                    Icon(
+                        painter = imgPhone,
+                        contentDescription = null,
+                        modifier = Modifier.size(30.dp),
+                        tint = Color.White
+                    )
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                Column {
+                    Text(
+                        text = "+34 612345678",
+                        fontSize = 16.sp,
+                        color = Color.White
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {//ICONO MAIL
+                Column {
+                    Icon(
+                        painter = imgMail,
+                        contentDescription = null,
+                        modifier = Modifier.size(22.dp),
+                        tint = Color.White
+                    )
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                Column {
+                    Text(
+                        text = "@Grupo4Dev",
+                        fontSize = 16.sp,
+                        color = Color.White
+                    )
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {//ICONO SHARE
+            Column {
+                Icon(
+                    painter = imgShare,
+                    contentDescription = null,
+                    modifier = Modifier.size(24.dp),
+                    tint = Color.White
+                )
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Column {
+                Text(
+                    text = "grupo4@ejemplo.com",
+                    fontSize = 16.sp,
+                    color = Color.White
+                )
+            }
+        }
     }
 }
